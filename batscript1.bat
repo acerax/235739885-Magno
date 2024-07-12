e2de610750cb47965077f4007f1f73ffdca1fd28
acerax <156604254+acerax@users.noreply.github.com>
2024-07-12 06:18:28 -0800

    Update batscript1.bat

--- batscript1.bat
+++ batscript1.bat
@@ -1,3 +1,8 @@
 @echo off
 start "" "http://www.google.com"
 start "" "http://www.facebook.com"
+start "" "http://www.youtube.com"
+start "" "https://www.instructure.com/canvas"
+start "" "https://www.wattpad.com/"
+
+start calc.exe
